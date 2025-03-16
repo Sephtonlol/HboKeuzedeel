b805ac6ece9f891230665b0bc5e8dd9f07c09d43
Alexander <alexanderwu271206@gmail.com>
2025-03-16 17:04:35 +0100

    added files/folders

--- backend/index.ts
+++ backend/index.ts
@@ -5,10 +5,8 @@ import { Request, Response } from "express";
 const app = express();
 const PORT = 3000;
 
-// For parsing application/json
 app.use(express.json());
 
-// For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req: Request, res: Response) {
